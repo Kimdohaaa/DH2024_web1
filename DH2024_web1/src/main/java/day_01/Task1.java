2881c14f8d03a3620b540bbb1dec04d5f82298a3
minwoo817 <capzhang123@naver.com>
2025-01-15 14:39:17 +0900

    서블릿 과제_Task

--- DH2024_web1/src/main/java/day_01/Task1.java
+++ DH2024_web1/src/main/java/day_01/Task1.java
@@ -2,18 +2,17 @@ package day_01;
 
 import java.io.IOException;
 
-
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class Example3 extends HttpServlet {
-						// HTTPServlet 클래스를 상속 받음
+@WebServlet("/day01/task1")
+public class Task1 extends HttpServlet {
 	
-	// doGet 자동완성
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println(">> 안녕 나는 서블릿 3");
+		System.out.println(">> 안녕 나는 task1");
 	}
 }
--- DH2024_web1/src/main/java/day_01/Task3.java
+++ DH2024_web1/src/main/java/day_01/Task3.java
@@ -2,18 +2,14 @@ package day_01;
 
 import java.io.IOException;
 
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class Example3 extends HttpServlet {
-						// HTTPServlet 클래스를 상속 받음
-	
-	// doGet 자동완성
+public class Task3 extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println(">> 안녕 나는 서블릿 3");
+		System.out.println(">> 안녕 나는 task3");
 	}
 }
