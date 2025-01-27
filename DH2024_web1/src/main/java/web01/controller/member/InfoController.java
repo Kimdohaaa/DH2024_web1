065466362cf054aa94a86877796bea6f99066755
Kimdohaaa <jjeenee0408@gmail.com>
2025-01-27 15:54:45 +0900

    내정보조회/회원탈퇴/회원수정(세션 이용)

--- DH2024_web1/src/main/java/web01/controller/member/InfoController.java
+++ DH2024_web1/src/main/java/web01/controller/member/InfoController.java
@@ -2,6 +2,8 @@ package web01.controller.member;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,6 +15,7 @@ import web01.model.dto.MemberDto;
 
 @WebServlet("/member/info")
 public class InfoController extends HttpServlet{
+	// [1] 내정보 조회
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(">> INFO GET");
@@ -27,7 +30,7 @@ public class InfoController extends HttpServlet{
 			// 2) 세션 객체 내 loginMno 속성값 가져오기
 		Object object = session.getAttribute("loginMno");
 		
-		// [2] 만약 세션 객체에 지정한 속성값이 존재하면 int 타입으로 다운캐스팅
+		// [2] 만약 세션 객체에 지정한 속성값이 존재하면 int 타입으로 다운캐스팅 후 DAO 에게 전달
 		if(object != null) {
 			// 1) 다운캐스팅
 			int loginMno = (Integer)object;
@@ -39,4 +42,66 @@ public class InfoController extends HttpServlet{
 		resp.setContentType("application/json");
 		resp.getWriter().print(result);
 	}
+	
+	// [2] 회원 탈퇴
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(">> MEMBER DELETE");
+		// 정보를 저장할 변수 생성 //
+		boolean result = false;
+		
+		// [1] 세션 객체 가져오기 (현재 로그인된 회원번호)
+		HttpSession session = req.getSession();
+		Object object = session.getAttribute("loginMno");
+		
+		// [3] 만약 세션 객체에 지정한 속성값이 존재 시 int 타입으로 다운캐스팅 후 DAO 에게 전달
+		if(object != null) {
+			int loginMno = (Integer)object;
+			result = MemberDao.getInstance().delete(loginMno);
+			
+			// [4] 만약 회원 탈퇴 성공 시 세션 값을 제거하여 자동 로그아웃			
+			if(result == true) {
+				session.removeAttribute("loginMno");
+			}
+		}
+		
+		// [5] HTTP HEADER BODY 에게 JSON 타입으로 결과 반환
+		resp.setContentType("application/json");
+		resp.getWriter().print(result);
+	}
+	
+	// [3] 회원 정보 수정
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(">> MEMBER PUT");
+
+		boolean result = false;
+		
+		// [1] HTTP HEADER BODY 자료(JSON) 자바(DTO) 타입으로 요청 받기
+		ObjectMapper mapper = new ObjectMapper();
+		MemberDto memberDto = mapper.readValue(req.getReader(), MemberDto.class);
+		
+		// [2] 유효성 검사
+		
+		// [3] 세션 객체 가져오기 (현재 로그인된 회원번호)
+		HttpSession session = req.getSession();
+		Object object = session.getAttribute("loginMno");
+		
+		// [4] 만약 세션 객체에 지정한 속성값이 존재 시 int 타입으로 다운캐스팅 후 DAO 에게 전달
+		if(object != null) {
+			int loginMno = (Integer)object;
+			memberDto.setMno(loginMno); // 세션 객체에서 조회된 loginMno 를 DTO 에 set
+			result = MemberDao.getInstance().update(memberDto);
+		}
+
+		// [5] HTTP HEADER BODY 에게 JSON 타입으로 결과 반환
+		resp.setContentType("application/json");
+		resp.getWriter().print(result);
+	}
+	
 }
+
+
+
+
+
