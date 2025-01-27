207236dac7fb21da67963f28a82b99166060730d
Kimdohaaa <jjeenee0408@gmail.com>
2025-01-27 10:32:41 +0900

    MemberDto(web)

---
+++ DH2024_web1/src/main/java/web01/model/dto/MemberDto.java
@@ -0,0 +1,24 @@
+package web01.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+// LOMBOK 라이브러리 어노테이션으로 생성자 / getter / setter / ToString 자동 생성 
+@NoArgsConstructor	
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberDto {
+	// 멤버 변수
+	private int mno;		// 회원번호
+	private String mid;		// 아이디 
+	private String mpwd;	// 비밀번호
+	private String mname;	// 이름
+	private String mphone;	// 연락처 
+	private String mdate;	// 가입일 
+	
+}		
