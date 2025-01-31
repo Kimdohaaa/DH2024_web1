18688680cd72aff807f2d7a000932886e2519682
Kimdohaaa <jjeenee0408@gmail.com>
2025-01-31 14:10:52 +0900

    대개번호발행 프로그램(세션이용)

--- DH2024_web1/src/main/java/day08/WaitingController.java
+++ DH2024_web1/src/main/java/day08/WaitingController.java
@@ -33,9 +33,10 @@ public class WaitingController extends HttpServlet{
 		// 세션 객체 불러오기
 		HttpSession session = req.getSession();
 		
+		// 세션 객체 -변환-> ArrayList<HashMap<String, String>>
 		ArrayList<HashMap<String, String>> list = (ArrayList<HashMap<String, String>>)session.getAttribute("list");
 
-		   
+		// 입력값 대입   
 		if (list == null) {
 			list = new ArrayList<>();
 			result = true;
@@ -47,6 +48,7 @@ public class WaitingController extends HttpServlet{
 		
 		session.setAttribute("wno", map.get("wno"));
 
+		// 응답
 		resp.setContentType("application/json");
 		resp.getWriter().print(result);
 		System.out.println(list);
@@ -56,7 +58,7 @@ public class WaitingController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(">> Waiting Get");
-
+		
 		ArrayList<HashMap<String, String>> list = null;
 		
 		Object object =  req.getSession().getAttribute("list");
@@ -69,8 +71,10 @@ public class WaitingController extends HttpServlet{
 
 		ObjectMapper mapper = new ObjectMapper();
 		
+		// JSON 타입으로 변환
 		String jsonResult = mapper.writeValueAsString(list);
 		
+		// 응답
 		resp.setContentType("application/json");
 		resp.getWriter().print(jsonResult);
 		System.out.println(jsonResult);	
@@ -80,28 +84,32 @@ public class WaitingController extends HttpServlet{
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(">> Waiting Delete");
-	
+		// 상태를 저장할 변수
 		boolean result = false;
 		
+		// 쿼리스트링으로 값 받아오기
 		String delete = req.getParameter("wno");
 		
+		// 세션 객체 불러오기
 		HttpSession session = req.getSession();
 		
-		
+		// 세션 객체 -변환-> ArrayList<HashMap<String, String>>
 		ArrayList<HashMap<String, String>> list = (ArrayList<HashMap<String, String>>)session.getAttribute("list");
 		
+		// 배열 순회를 통해 쿼리스트링으로 입력 받은 값 DELETE
 		if (list != null && delete != null) {
 	        Iterator<HashMap<String, String>> iterator = list.iterator();
-	        while (iterator.hasNext()) {
+	        while (iterator.hasNext()) {	// 모든 값 조회
 	            HashMap<String, String> map = iterator.next();
-	            if (map.get("wno").equals(delete)) {
-	                iterator.remove();
-	                result = true;
-	                break; 
+	            if (map.get("wno").equals(delete)) {	// 만약 쿼리스트링으로 입력받은 값과 같으면 
+	                iterator.remove();	// DELETE
+	                result = true;		// 상태를 저장하는 변수 true 로 변경
+	                break; 				// 반복 종료
 	            }
 	        }
 	    }
 		
+		// 응답
 		resp.setContentType("application/json");
 		resp.getWriter().print(result);
 	}
@@ -109,29 +117,33 @@ public class WaitingController extends HttpServlet{
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(">> Waiting Put");
-		
+		// 상태를 저장할 변수
 		boolean result = false;
-		
+		// 요청받기
 		ObjectMapper mapper = new ObjectMapper();
 		HashMap<String, String> map = mapper.readValue(req.getReader(), HashMap.class);
 		String wno = map.get("wno");
 		
+		// 세션 객체 불러오기
 		HttpSession session = req.getSession();
 				
+		// 세션 객체 -변환-> ArrayList<HashMap<String, String>>
 		ArrayList<HashMap<String, String>> list = (ArrayList<HashMap<String, String>>)session.getAttribute("list");
 		
-		
+		// 배열 순회
 	    if (list != null) {
 	        for (HashMap<String, String> index : list) {
-	            if (index.get("wno").equals(wno)) {
-	                index.put("wcount", map.get("wcount"));
-	                session.setAttribute("list", list);
-	                result = true;
-	                break;
+	            if (index.get("wno").equals(wno)) {	// 입력받은 값과 같으면
+	                index.put("wcount", map.get("wcount"));	// 값 변경
+	                session.setAttribute("list", list);		// 세션 값 변경
+	                result = true;							// 상태를 저장하는 변수 true 로 변경
+	                break;									// 반복 종료
 	            }
 	        }
 	    }
-
+	    
+	    
+	    // 응답
 		resp.setContentType("application/json");
 		resp.getWriter().print(result);
 		
