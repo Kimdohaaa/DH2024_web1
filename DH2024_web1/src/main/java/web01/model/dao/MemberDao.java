1f8b846b05de69a62d8355755720ad41c48a782b
Kimdohaaa <jjeenee0408@gmail.com>
2025-02-04 15:42:32 +0900

    MemberDao(+Point 기능)

--- DH2024_web1/src/main/java/web01/model/dao/MemberDao.java
+++ DH2024_web1/src/main/java/web01/model/dao/MemberDao.java
@@ -3,10 +3,12 @@ package web01.model.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import web01.model.dto.MemberDto;
+import web01.model.dto.PointDto;
 
 //@Getter // -> 클래스 내 모든 멤버변수에 어노테이션으로 getter 지정
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
@@ -19,7 +21,7 @@ public class MemberDao extends Dao{
 	private static MemberDao instance = new MemberDao();
 
 	// 싱글톤 e
-	// 1. 회원가입 SQL 처리 메소드 
+	// [1] 회원가입 SQL 처리 메소드 
 	public boolean signup( MemberDto memberDto ) {
 		try {
 			// [1] SQL 작성한다.
@@ -46,7 +48,7 @@ public class MemberDao extends Dao{
 		
 	} // f end
 	
-	// 2. 로그인 SQL 처리 메소드
+	// [2] 로그인 SQL 처리 메소드
 	public int login( MemberDto memberDto ) {
 		// int : SQL로 조회된 회원번호를 반환하기 위해서
 		try {
@@ -71,31 +73,63 @@ public class MemberDao extends Dao{
 	
 	// [3] 내정보 보기 SQL 처리 메소드 
     public MemberDto myInfo( int loginMno ) {
-            try {
-                    String sql ="select * from member where mno = ? ";
+    	    try {
+                    String sql ="select p.mno , m.mid, m.mname, m.mphone, m.mdate, m.mimg , p.pcount, p.pname"
+                    		+ "	from member m join point p on m.mno = p.mno where p.mno = ? ";
                     // [1] SQL 작성한다.
                     PreparedStatement ps = conn.prepareStatement(sql); 
                     // [2] DB와 연동된 곳에 SQL 기재한다.
                     ps.setInt(  1 , loginMno); // [*] 기재된 SQL 에 매개변수 값 대입한다.
                     ResultSet rs = ps.executeQuery(); 
+                    MemberDto memberDto = new MemberDto();
+                    
                     // [3] 기재된 SQL 실행하고 결과를 받는다.
                     if( rs.next() ) { 
                     		// [4] 결과에 따른 처리 및 반환를 한다.
-                            MemberDto memberDto = new MemberDto();
                             memberDto.setMno(rs.getInt("mno"));
                             memberDto.setMid( rs.getString("mid") );
                             memberDto.setMname( rs.getString("mname" ) );
                             memberDto.setMphone( rs.getString("mphone") );
                             memberDto.setMdate( rs.getString("mdate") );
                             memberDto.setMimg(rs.getString("mimg"));
-                            
-                            return memberDto; // 조회된 회원정보를 반환한다.
+                    
+                            return memberDto;
                     }
             }catch(SQLException e ) { System.out.println(e);}
+            
             return null; // 조회된 회원정보가 없을때. null 반환한다
     } // f end
     
-    // [4] 회원탈퇴
+    // [4] 현재 로그인된 회원의 포인트 내역
+    public ArrayList<PointDto> pointList( int loginMno ) {
+    	ArrayList<PointDto> list = new ArrayList<PointDto>();
+            try {
+                    String sql ="select p.mno , m.mid, m.mname, m.mphone, m.mdate, m.mimg , p.pcount, p.pname"
+                    		+ "	from point p join member m on m.mno = p.mno where p.mno = ? ";
+                    // [1] SQL 작성한다.
+                    PreparedStatement ps = conn.prepareStatement(sql); 
+                    // [2] DB와 연동된 곳에 SQL 기재한다.
+                    ps.setInt(  1 , loginMno); // [*] 기재된 SQL 에 매개변수 값 대입한다.
+                    ResultSet rs = ps.executeQuery(); 
+                    // [3] 기재된 SQL 실행하고 결과를 받는다.
+                    while( rs.next() ) { 
+                    	PointDto pointDto =  new PointDto(); 
+                        
+                    	// [4] 결과에 따른 처리 및 반환를 한다.
+                            pointDto.setPname(rs.getString("pname"));
+                            pointDto.setPcount(rs.getInt("pcount"));
+                            
+                            System.out.println(pointDto.getPcount());
+                            System.out.println(pointDto.getPname());
+                            
+                            list.add(pointDto);
+                    }
+            }catch(SQLException e ) { System.out.println(e);}
+            
+            return list; // 조회된 회원정보가 없을때. null 반환한다
+    } // f end
+    
+    // [5] 회원탈퇴
     public boolean delete( int loginMno ) {
 		try {
 			String sql = "delete from member where mno = ? ";
@@ -107,7 +141,7 @@ public class MemberDao extends Dao{
 		return false;
 	} // f end "					
 	
-    // [5] 회원수정  SQL 처리 메소드
+    // [6] 회원수정  SQL 처리 메소드
 	public boolean update( MemberDto memberDto ) {
 		try {
 			// [1] SQL 작성한다.
@@ -153,7 +187,7 @@ public class MemberDao extends Dao{
 		}
 	}
 	
-	// [6] 회원가입 포인트 지급
+	// [7] 회원가입 포인트 지급
 	public boolean pointIn(int mno) {
 		try {
 
@@ -190,6 +224,36 @@ public class MemberDao extends Dao{
 			System.out.println();
 		}
 		return false;
+	}
+	
+	// [9] 로그인된 회원의 정보 조회(아이디 / 전화번호 / 프로필 / 현재 총 포인트)
+	public MemberDto totalP(int loginMno) {
+		
+		try {
+			String sql = "select p.mno , m.mid, m.mname, m.mphone, m.mdate, m.mimg , sum( p.pcount ) total"
+					+ "	from member as m join point as p on m.mno = p.mno where p.mno = ? group by  m.mno, m.mid, m.mname, m.mphone, m.mdate, m.mimg";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, loginMno);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				
+				MemberDto memberDto = new MemberDto();
+				
+				memberDto.setMphone( rs.getString("mphone") );
+                memberDto.setMid(rs.getString("mid"));
+                memberDto.setMimg(rs.getString("mimg"));
+                memberDto.setTotal(rs.getInt("total"));
+                
+                return memberDto;
+                
+			}
+		}catch (SQLException e) {
+			System.out.println(e);
+		}
+		return null;
 	}
 }
 
