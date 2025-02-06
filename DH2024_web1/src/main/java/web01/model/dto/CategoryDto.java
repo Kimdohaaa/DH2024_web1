9e695a49ad235afa85328f8460893615ede8ea81
Kimdohaaa <jjeenee0408@gmail.com>
2025-02-06 09:47:39 +0900

    카테고리 DTO

---
+++ DH2024_web1/src/main/java/web01/model/dto/CategoryDto.java
@@ -0,0 +1,17 @@
+package web01.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class CategoryDto {
+	private int cno;
+	private String cname;
+}
